6aafd243eff1f0066df38fa20148a289553f130e
hejinyu <hejinyu@rongcloud.cn>
2019-04-02 10:56:00 +0800

    fix:文件引入

--- RongCloud/Autoloader.php
+++ RongCloud/Autoloader.php
@@ -1,7 +1,7 @@
 <?php
 function __autoload($class){
-    if(is_file(RONGCLOUOD_ROOT.'../\\'."$class.php")){
-        require(RONGCLOUOD_ROOT.'../\\'."$class".'.'.'php');
+    if(is_file(str_replace("\\", "/", RONGCLOUOD_ROOT.'../'."$class".'.'.'php'))){
+        require(str_replace("\\", "/", RONGCLOUOD_ROOT.'../'."$class".'.'.'php'));
     }
 }
 spl_autoload_register("__autoload");
