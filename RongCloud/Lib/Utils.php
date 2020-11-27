ba9728e68330895630a6b8b6722093939cb6ae67
chiquanqing <chiquanqing@yunjingwang.cn>
2020-11-27 14:59:27 +0800

    修复因为使用 empty 进行字段检测导致传字符串0的时候，会被判断为空的问题

--- RongCloud/Lib/Utils.php
+++ RongCloud/Lib/Utils.php
@@ -1,7 +1,9 @@
 <?php
+
 /**
  * 工具类
  */
+
 namespace RongCloud\Lib;
 
 class Utils
@@ -14,9 +16,9 @@ class Utils
     public function isLength($params)
     {
         $val = $params['val'];
-        $min = isset($params['min'])?$params['min']:0;
-        $len = !is_array($val)?strlen($val):count($val);
-        $max = isset($params['max'])?$params['max']:0;
+        $min = isset($params['min']) ? $params['min'] : 0;
+        $len = !is_array($val) ? strlen($val) : count($val);
+        $max = isset($params['max']) ? $params['max'] : 0;
         $isLen = false;
         if ($len < $min || $len > $max) {
             $isLen = true;
@@ -30,10 +32,11 @@ class Utils
      * @param array $array
      * @return mixed
      */
-    public function rename($obj,$array=[]){
-        foreach ($array as $key=>$v){
-            if(isset($obj[$key])){
-                $obj[$v] = isset($obj[$key])?$obj[$key]:"";
+    public function rename($obj, $array = [])
+    {
+        foreach ($array as $key => $v) {
+            if (isset($obj[$key])) {
+                $obj[$v] = isset($obj[$key]) ? $obj[$key] : "";
                 unset($obj[$key]);
             }
         }
@@ -46,12 +49,11 @@ class Utils
      * @param $data
      * @return mixed
      */
-    public function tplEngine ($temp,$data)
+    public function tplEngine($temp, $data)
     {
-        foreach ($data as $k=>$v)
-        {
-            $v = is_array($v)|| is_object($v)?"object":$v;
-            $temp = str_replace("{{{$k}}}",$v, $temp);
+        foreach ($data as $k => $v) {
+            $v = is_array($v) || is_object($v) ? "object" : $v;
+            $temp = str_replace("{{{$k}}}", $v, $temp);
         }
         return $temp;
     }
@@ -65,20 +67,19 @@ class Utils
     {
         $code = $params['code'];
         $errorInfo = [];
-        if (is_array($code))
-        {
-            $errorInfo = $this->rename($code, ['errorMessage'=> 'msg']);
+        if (is_array($code)) {
+            $errorInfo = $this->rename($code, ['errorMessage' => 'msg']);
             $code = $errorInfo['code'];
         }
         $errors = $params['errors'];
-        $info = isset($params['info'])?$params['info']:[];
-    
-        $error = isset($errors[$code])?$errors[$code]:[];
+        $info = isset($params['info']) ? $params['info'] : [];
+
+        $error = isset($errors[$code]) ? $errors[$code] : [];
         if (!$error) {
             $error = $errorInfo;
         }
-        if(isset($error['msg'])){
-            $error['msg'] = $code ? $this->tplEngine($error['msg'], $info):$errorInfo['msg'];
+        if (isset($error['msg'])) {
+            $error['msg'] = $code ? $this->tplEngine($error['msg'], $info) : $errorInfo['msg'];
         }
         return $error;
     }
@@ -88,29 +89,29 @@ class Utils
      * @param $params
      * @return array|mixed|null
      */
-    public function checkLength ($params)
+    public function checkLength($params)
     {
         $proto = $params['proto'];
         $verify = ($params['verify'][$proto]['length']);
         $val = $params['val'];
         $errors = $params['response'];
-    
+
         $isLen = $this->isLength([
-            'max'=> $verify['max'],
-            'min'=>$verify['min'],
-            'val'=> $val
+            'max' => $verify['max'],
+            'min' => $verify['min'],
+            'val' => $val
         ]);
-    
+
         $error = null;
         if ($isLen) {
             $error = $this->getError([
-                'code'=> $verify['invalid'],
-                'errors'=> $errors,
-                'info'=> [
-                    'name'=> $proto,
-                    'max'=> $verify['max'],
-                    'min'=> $verify['min'],
-                    'len'=> substr($val,0,$verify['max'])
+                'code' => $verify['invalid'],
+                'errors' => $errors,
+                'info' => [
+                    'name' => $proto,
+                    'max' => $verify['max'],
+                    'min' => $verify['min'],
+                    'len' => substr($val, 0, $verify['max'])
                 ]
             ]);
         }
@@ -130,20 +131,20 @@ class Utils
         $val = $params['val'];
         $errors = $params['response'];
         $isFalse = 0;
-        if($verify['type'] == "array" && !is_array($val)){
+        if ($verify['type'] == "array" && !is_array($val)) {
             $isFalse = 1;
         }
-        if($verify['type'] == "number" && !is_numeric($val)){
+        if ($verify['type'] == "number" && !is_numeric($val)) {
             $isFalse = 1;
         }
-        if($isFalse){
+        if ($isFalse) {
             $error = $this->getError([
-                'code'=> $verify['invalid'],
-                'errors'=> $errors,
-                'info'=> [
-                    'currentType'=> gettype($val),
-                    'name'=> $proto,
-                    'type'=> $verify['type']
+                'code' => $verify['invalid'],
+                'errors' => $errors,
+                'info' => [
+                    'currentType' => gettype($val),
+                    'name' => $proto,
+                    'type' => $verify['type']
                 ]
             ]);
         }
@@ -162,31 +163,31 @@ class Utils
         $api = $params['api'];
         $errors = $api['response']['fail'];
         $dataModel = $api['params'][$model];
-        $data = isset($params['data'])?$params['data']:[];
+        $data = isset($params['data']) ? $params['data'] : [];
         $verify = $params['verify'];
         $checkMap = [
-                'length'=>'checkLength',
-                'require'=>'checkRequire',
-                'typeof'=>'checkTypeof',
-                'size'=>'checkSize'
+            'length' => 'checkLength',
+            'require' => 'checkRequire',
+            'typeof' => 'checkTypeof',
+            'size' => 'checkSize'
         ];
         $isBreak = false;
-        foreach($dataModel as $proto=>$val ){
+        foreach ($dataModel as $proto => $val) {
             if ($isBreak) {
                 break;
             }
-            $protoVerify = isset($verify[$proto])?$verify[$proto]:"";
+            $protoVerify = isset($verify[$proto]) ? $verify[$proto] : "";
             if ($protoVerify) {
-                foreach( $protoVerify as $rule=>$ruleVal){
+                foreach ($protoVerify as $rule => $ruleVal) {
                     $fun = $checkMap[$rule];
                     $error = $this->$fun([
-                        'verify'=>$verify,
-                        'proto'=> $proto,
-                        'val'=> isset($data[$proto])?$data[$proto]:"",
-                        'response'=>$errors
+                        'verify' => $verify,
+                        'proto' => $proto,
+                        'val' => isset($data[$proto]) ? $data[$proto] : "",
+                        'response' => $errors
                     ]);
                     if ($error) {
-//                        var_dump($errors,$fun,$verify,$data,$proto);
+                        // var_dump($errors,$fun,$verify,$data,$proto);
                         $isBreak = true;
                         break;
                     }
@@ -201,30 +202,30 @@ class Utils
      * @param $params
      * @return array|mixed|null
      */
-    function checkSize ($params)
+    function checkSize($params)
     {
         $error = null;
         $proto = $params['proto'];
         $verify = $params['verify'][$proto]['size'];
         $size = $params['val'];
-        if(is_array($params['val'])){
+        if (is_array($params['val'])) {
             $size = count($params['val']);
         }
-        if(is_string($params['val'])){
-            $size =strlen($params['val']);
+        if (is_string($params['val'])) {
+            $size = strlen($params['val']);
         }
         $errors = $params['response'];
         $isIllegal = ($size < $verify['min'] || $size > $verify['max']);
         if ($isIllegal) {
             $error = $this->getError([
-                'code'=> $verify['invalid'],
-                'errors'=> $errors,
-                'info'=> [
-                    'size'=> $size
+                'code' => $verify['invalid'],
+                'errors' => $errors,
+                'info' => [
+                    'size' => $size
                 ]
             ]);
         }
-    
+
         return $error;
     }
 
@@ -233,21 +234,26 @@ class Utils
      * @param $params
      * @return array|mixed|null
      */
-    function checkRequire ($params)
+    function checkRequire($params)
     {
         $error = null;
         $proto = $params['proto'];
         $verify = $params['verify'][$proto]['require'];
         $errors = $params['response'];
-        $isIllegal = empty($params['val'])?1:0;
-        if($isIllegal){
+        $isIllegal = empty($params['val']) ? 1 : 0;
+        if (is_array($params['val'])) {
+            $isIllegal = empty($params['val']) ? 1 : 0;
+        } else {
+            $isIllegal = trim($params['val']) == '' ? 1 : 0;
+        }
+        if ($isIllegal) {
             $error = $this->getError([
-                            'code'=>$verify['invalid'],
-                            'errors'=>$errors,
-                            'info'=>[
-                                'name'=>$proto
-                            ]
-                        ]);
+                'code' => $verify['invalid'],
+                'errors' => $errors,
+                'info' => [
+                    'name' => $proto
+                ]
+            ]);
         }
 
         return $error;
@@ -258,9 +264,9 @@ class Utils
      * @param string $path
      * @return mixed
      */
-    public static function getJson($path="")
+    public static function getJson($path = "")
     {
-        $files = file_get_contents(RONGCLOUOD_ROOT.$path);
+        $files = file_get_contents(RONGCLOUOD_ROOT . $path);
         return json_decode($files, true);
     }
     /**
@@ -270,24 +276,25 @@ class Utils
      * @version php>=5.6
      * @return void
      */
-    public static function dump(){
-		$param = func_get_args();
+    public static function dump()
+    {
+        $param = func_get_args();
         echo '<style>.php-print{background:#eee;padding:10px;border-radius:4px;border:1px solid #ccc;line-height:1.5;white-space:pre-wrap;font-family:Menlo,Monaco,Consolas,"Courier New",monospace;font-size:13px;}</style>', '<pre class="php-print">';
 
-        if(end($param) === .1){
+        if (end($param) === .1) {
             array_splice($param, -1, 1);
 
-            foreach($param as $k => $v){
-                echo $k>0 ? '<hr>' : '';
+            foreach ($param as $k => $v) {
+                echo $k > 0 ? '<hr>' : '';
 
                 ob_start();
                 Utils::dump($v);
 
                 echo preg_replace('/]=>\s+/', '] => <label>', ob_get_clean());
             }
-        }else{
-            foreach($param as $k => $v){
-                echo $k>0 ? '<hr>' : '', print_r($v, true);
+        } else {
+            foreach ($param as $k => $v) {
+                echo $k > 0 ? '<hr>' : '', print_r($v, true);
             }
         }
         echo '</pre>';
@@ -300,20 +307,21 @@ class Utils
      * @param array $failList 错误信息列表
      * @return array
      */
-    public function responseError($result = array(), $failList = array()){
-       if(!is_array($result)){
+    public function responseError($result = array(), $failList = array())
+    {
+        if (!is_array($result)) {
             $result = json_decode($result, true);
         }
-        if(isset($result['code']) && $result['code'] != 200){
+        if (isset($result['code']) && $result['code'] != 200) {
             unset($result['url']);
-			if($result['code'] == 1002){
-				unset($failList[1002]);
-			}
+            if ($result['code'] == 1002) {
+                unset($failList[1002]);
+            }
             $result['code'] = $result;
             $result['errors'] = $failList;
             $result = $this->getError($result);
             return $result;
-        }else{
+        } else {
             return $result;
         }
     }
@@ -323,7 +331,8 @@ class Utils
      * @param int $len
      * @return string
      */
-    public static function createRand($len = 6) {
+    public static function createRand($len = 6)
+    {
         $string = 'abcdefghijklmnopqrstuvwxyz0123456789';
         $result = '';
         $string = str_shuffle($string);
@@ -332,6 +341,4 @@ class Utils
         }
         return $result;
     }
-
-
 }
