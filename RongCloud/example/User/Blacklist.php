c56e155a3f461c4009b7b8507f480f751ad50c91
迟权庆 <chiquanqing@rongcloud.cn>
2024-12-18 17:58:09 +0800

    feat: 用户黑白名单增加参数注释说明

--- RongCloud/example/User/Blacklist.php
+++ RongCloud/example/User/Blacklist.php
@@ -48,6 +48,8 @@ function getList()
     $RongSDK = new RongCloud(APPKEY,APPSECRET);
     $user = [
         'id'=> 'ujadk90ha',//用户 id
+        'size'=> 1000,//分页获取黑名单用户列表时每页行数，不传时默认为 1000条，最大不超过 1000 条
+        'pageToken'=> ''//分页信息，上一次请求返回 next ，不传时不做分页处理，默认获取前 1000 个用户列表，按加入黑名单时间倒序排序。
     ];
     $Blacklist = $RongSDK->getUser()->Blacklist()->getList($user);
     Utils::dump("用户黑名单列表",$Blacklist);
